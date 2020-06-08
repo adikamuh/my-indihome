6948ec22d8bfb9ca28c2e66074c40c4c3283687a
lenopop <adikamuhammad1998@gmail.com>
2020-06-09 01:34:25 +0700

    add avatar, phone icon

--- src/components/profile-preview/index.js
+++ src/components/profile-preview/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 import './profile-preview.scss'
 
 import rightArrow from '../../assets/ic-chevron-white.svg'
+import avatar from '../../assets/avatar.png'
 
 export default class ProfilePreview extends React.Component {
     constructor(){
@@ -18,11 +19,14 @@ export default class ProfilePreview extends React.Component {
         return(
             <div className="profile-preview-container">
                 <div className="profile-preview-wrapper">
-                    <div className="avatar">
-
-                    </div>
+                    <div 
+                        className="avatar"
+                        style={{
+                            backgroundImage: 'url('+avatar+')'
+                        }}
+                    ></div>
                     <div className="content">
-                        <p className="greeting">{this.state.greeting}</p>
+                        <p className="greeting" className="title-small">{this.state.greeting}</p>
                         <a className="profile" href="profile">
                             <p>{this.state.name}</p>
                             <img src={rightArrow} alt=""></img>
