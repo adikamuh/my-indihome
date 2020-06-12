aa24cf60f2c290051f474674e5561abd5b265fad
adikamuh <adikamuhammad1998@gmail.com>
2020-06-12 17:38:18 +0700

    font

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './assets/fonts/Ubuntu-Regular.ttf'
+import './assets/fonts/Ubuntu-Bold.ttf'
 
 ReactDOM.render(
   <React.StrictMode>
