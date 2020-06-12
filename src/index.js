6d658f9c28613ae6f16017ab7bb67c488dde3f5b
adikamuh <adikamuhammad1998@gmail.com>
2020-06-12 17:36:54 +0700

    add ubuntu font

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 // import './all.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './assets/fonts/Ubuntu-Regular.ttf'
 
 ReactDOM.render(
   <React.StrictMode>
